f4d41c19f5f80f6b4735dc3388f98b848f53f06a
aggie-ch <aggie-ch@github.com>
2020-04-27 13:22:06 +0200

    Added @annotation and ControllerAdvice

---
+++ src/main/java/io/github/aggie/medicalapp/controller/IllegalExceptionProcessing.java
@@ -0,0 +1,9 @@
+package io.github.aggie.medicalapp.controller;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface IllegalExceptionProcessing {
+
+}
--- src/main/java/io/github/aggie/medicalapp/controller/MedicationGroupController.java
+++ src/main/java/io/github/aggie/medicalapp/controller/MedicationGroupController.java
@@ -84,16 +84,6 @@ class MedicationGroupController {
         return ResponseEntity.ok(medicationRepository.findByGroupId(id));
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(IllegalStateException.class)
-    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
     @ModelAttribute("groups")
     List<GroupReadModel> getGroups() {
         return groupService.readAll();
